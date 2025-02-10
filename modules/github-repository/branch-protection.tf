9375521d0afaa1b87a2460c8af54799df7857040
Peter Pathirana <peter@latent.io>
2025-02-10 05:38:55 +0000

    feat(github-repository): add module github-repository

---
+++ modules/github-repository/branch-protection.tf
@@ -0,0 +1,16 @@
+resource "github_branch_protection" "main" {
+  repository_id                   = github_repository.repository.node_id
+  pattern                         = "main"
+  allows_deletions                = false
+  allows_force_pushes             = false
+  enforce_admins                  = true
+  force_push_bypassers            = []
+  require_conversation_resolution = false
+  required_linear_history         = true
+  require_signed_commits          = true
+
+  required_status_checks {
+    strict   = true
+    contexts = var.required_status_checks
+  }
+}
---
+++ modules/github-repository/variables.tf
@@ -0,0 +1,27 @@
+variable "actions_secrets" {
+  type    = map(string)
+  default = {}
+}
+
+variable "actions_allowed" {
+  type    = list(string)
+  default = []
+}
+
+variable "repository" {
+  type = object({
+    name        = string
+    description = string
+    visibility  = string
+  })
+}
+
+variable "required_status_checks" {
+  type    = list(string)
+  default = []
+}
+
+variable "topics" {
+  type    = list(string)
+  default = []
+}
