9e39cc8e9740c8b48581d6210ce25e91496d598a
Peter Pathirana <peter@latent.io>
2025-06-05 00:24:33 -0400

    feat(github): add github repo - dotfiles (#74)

---
+++ workspaces/github/repo-dotfiles.tf
@@ -0,0 +1,30 @@
+module "repo_dotfiles" {
+  source = "../../modules/github-repository"
+  repository = {
+    name        = "dotfiles"
+    description = ""
+    visibility  = "public"
+  }
+  actions_allowed = [
+    "aquaproj/aqua-installer@*",
+    "aquaproj/update-checksum-action@*",
+    "aquaproj/update-checksum-workflow/.github/workflows/update-checksum.yaml@*",
+    "jdx/mise-action@*",
+    "suzuki-shunsuke/github-token-action@*",
+    "tibdex/github-app-token@*",
+    "tj-actions/changed-files@*"
+  ]
+  actions_secrets = {
+    HOMELAB_BOT_APP_ID          = data.bitwarden_secret.homelab_bot_app_id.value
+    HOMELAB_BOT_APP_PRIVATE_KEY = data.bitwarden_secret.homelab_bot_app_private_key.value
+  }
+  environment_secrets = {
+    renovate = {
+      DOCKERHUB_USERNAME       = data.bitwarden_secret.dockerhub_username.value
+      DOCKERHUB_TOKEN          = data.bitwarden_secret.dockerhub_token.value
+      RENOVATE_APP_ID          = data.bitwarden_secret.renovate_app_id.value
+      RENOVATE_APP_PRIVATE_KEY = data.bitwarden_secret.renovate_app_private_key.value
+    }
+  }
+  force_push_bypassers = [data.github_user.current.node_id]
+}
