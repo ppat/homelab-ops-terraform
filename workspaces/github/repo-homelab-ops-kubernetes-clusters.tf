508cec8b8edb7e42c7cc2dc5b9c5e2083d9d9087
Peter Pathirana <peter@latent.io>
2025-06-23 17:39:15 +0000

    feat(github): add homelab-ops-kubernetes-clusters repo

---
+++ workspaces/github/repo-homelab-ops-kubernetes-clusters.tf
@@ -0,0 +1,33 @@
+module "repo_homelab_ops_kubernetes_clusters" {
+  source = "../../modules/github-repository"
+  repository = {
+    name        = "homelab-ops-kubernetes-clusters"
+    description = "Kubernetes Cluster configuration for my homelab"
+    visibility  = "public"
+  }
+  actions_allowed = [
+    "allenporter/flux-local/action/diff@*",
+    "fluxcd/flux2/action@*",
+    "fluxcd/pkg/actions/kubeconform@*",
+    "fluxcd/pkg/actions/kubectl@*",
+    "fluxcd/pkg/actions/kustomize@*",
+    "googleapis/release-please-action@*",
+    "helm/kind-action@*",
+    "jdx/mise-action@*",
+    "kyverno/action-install-chainsaw@*",
+    "mshick/add-pr-comment@*",
+    "tj-actions/changed-files@*"
+  ]
+  environment_secrets = {
+    release = {
+      HOMELAB_BOT_APP_ID          = data.bitwarden_secret.homelab_bot_app_id.value
+      HOMELAB_BOT_APP_PRIVATE_KEY = data.bitwarden_secret.homelab_bot_app_private_key.value
+    }
+    renovate = {
+      DOCKERHUB_USERNAME       = data.bitwarden_secret.dockerhub_username.value
+      DOCKERHUB_TOKEN          = data.bitwarden_secret.dockerhub_token.value
+      RENOVATE_APP_ID          = data.bitwarden_secret.renovate_app_id.value
+      RENOVATE_APP_PRIVATE_KEY = data.bitwarden_secret.renovate_app_private_key.value
+    }
+  }
+}
