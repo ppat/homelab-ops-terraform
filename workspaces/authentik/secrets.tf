8a7d892186001bb1a9835b2b59e0ae34e31cafb5
Peter Pathirana <peter@latent.io>
2025-07-14 21:25:19 +0000

    feat(authentik): configure authentik outpost on nas cluster

--- workspaces/authentik/secrets.tf
+++ workspaces/authentik/secrets.tf
@@ -1,3 +1,7 @@
 data "bitwarden_secret" "dns_zone" {
   id = "288eeda0-d57f-4a91-8651-b2090163ecc0"
 }
+
+data "bitwarden_secret" "authentik_nas_apiservertoken" {
+  id = "416dd0ce-4c18-47a0-987c-b31a01673a73"
+}
