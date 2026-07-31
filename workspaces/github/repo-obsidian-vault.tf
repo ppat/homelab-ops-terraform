d51addf83b2d2af150ee69b609f2817aac471124
Peter Pathirana <peter@latent.io>
2026-07-31 19:11:54 +0000

    fix: fix

--- workspaces/github/repo-obsidian-vault.tf
+++ workspaces/github/repo-obsidian-vault.tf
@@ -5,17 +5,4 @@ module "repo_obsidian_vault" {
     description = ""
     visibility  = "private"
   }
-  actions_allowed = [
-    "googleapis/release-please-action@*",
-    "jdx/mise-action@*",
-    "tj-actions/changed-files@*"
-  ]
-  actions_secrets = {
-    DOCKERHUB_USERNAME          = data.bitwarden_secret.dockerhub_username.value
-    DOCKERHUB_TOKEN             = data.bitwarden_secret.dockerhub_token.value
-    HOMELAB_BOT_APP_ID          = data.bitwarden_secret.homelab_bot_app_id.value
-    HOMELAB_BOT_APP_PRIVATE_KEY = data.bitwarden_secret.homelab_bot_app_private_key.value
-    RENOVATE_APP_ID             = data.bitwarden_secret.renovate_app_id.value
-    RENOVATE_APP_PRIVATE_KEY    = data.bitwarden_secret.renovate_app_private_key.value
-  }
 }
