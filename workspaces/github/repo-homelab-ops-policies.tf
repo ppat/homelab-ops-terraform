0bc810b9ce070e1fd462af336fa5b1e6f1391435
Peter Pathirana <peter@latent.io>
2026-08-24 15:17:10 -0400

    feat(github): create homelab-ops-policies repo (#311)

---
+++ workspaces/github/repo-homelab-ops-policies.tf
@@ -0,0 +1,21 @@
+module "repo_homelab_ops_policies" {
+  source = "../../modules/github-repository"
+  repository = {
+    name        = "homelab-ops-policies"
+    description = ""
+    visibility  = "public"
+  }
+  actions_allowed = [
+    "docker://ghcr.io/allenporter/flux-local:*",
+    "jdx/mise-action@*",
+    "tj-actions/changed-files@*"
+  ]
+  actions_secrets = {
+    DOCKERHUB_USERNAME          = data.bitwarden_secret.dockerhub_username.value
+    DOCKERHUB_TOKEN             = data.bitwarden_secret.dockerhub_token.value
+    HOMELAB_BOT_APP_ID          = data.bitwarden_secret.homelab_bot_app_id.value
+    HOMELAB_BOT_APP_PRIVATE_KEY = data.bitwarden_secret.homelab_bot_app_private_key.value
+    RENOVATE_APP_ID             = data.bitwarden_secret.renovate_app_id.value
+    RENOVATE_APP_PRIVATE_KEY    = data.bitwarden_secret.renovate_app_private_key.value
+  }
+}
