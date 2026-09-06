e13646eca475a80478e13590a9afeb5b79a2faf1
Peter Pathirana <peter@latent.io>
2026-09-05 20:04:10 -0400

    feat(github): add new repo - mediated-mailbox-mcp (#326)

---
+++ workspaces/github/repo-mediated-mailbox-mcp.tf
@@ -0,0 +1,38 @@
+module "repo_mediated_mailbox_mcp" {
+  source = "../../modules/github-repository"
+  repository = {
+    name        = "mediated-mailbox-mcp"
+    description = ""
+    visibility  = "private"
+  }
+  actions_allowed = [
+    "docker/build-push-action@*",
+    "docker/setup-buildx-action@*",
+    "docker/login-action@*",
+    "docker/metadata-action@*",
+    "docker/setup-qemu-action@*",
+    "googleapis/release-please-action@*",
+    "jdx/mise-action@*",
+    "tailscale/github-action@*",
+    "tj-actions/changed-files@*"
+  ]
+  actions_secrets = {
+    CONTAINER_REGISTRY          = "harbor.nas.${data.bitwarden_secret.dns_zone.value}"
+    CONTAINER_REGISTRY_USERNAME = data.bitwarden_secret.harbor_robot_username.value
+    CONTAINER_REGISTRY_PASSWORD = data.bitwarden_secret.harbor_robot_password.value
+    DOCKERHUB_USERNAME          = data.bitwarden_secret.dockerhub_username.value
+    DOCKERHUB_TOKEN             = data.bitwarden_secret.dockerhub_token.value
+    HOMELAB_BOT_APP_ID          = data.bitwarden_secret.homelab_bot_app_id.value
+    HOMELAB_BOT_CLIENT_ID       = data.bitwarden_secret.homelab_bot_client_id.value
+    HOMELAB_BOT_APP_PRIVATE_KEY = data.bitwarden_secret.homelab_bot_app_private_key.value
+    RENOVATE_APP_ID             = data.bitwarden_secret.renovate_app_id.value
+    RENOVATE_CLIENT_ID          = data.bitwarden_secret.renovate_client_id.value
+    RENOVATE_APP_PRIVATE_KEY    = data.bitwarden_secret.renovate_app_private_key.value
+    TAILSCALE_OAUTH_CLIENT_ID   = data.bitwarden_secret.tailscale_githubactionsci_clientid.value
+    TAILSCALE_OAUTH_SECRET      = data.bitwarden_secret.tailscale_githubactionsci_clientsecret.value
+  }
+  actions_variables = {
+    CONTAINER_REGISTRY_CACHE_PATH = "build-cache"
+    CONTAINER_REGISTRY_PATH       = "library"
+  }
+}
